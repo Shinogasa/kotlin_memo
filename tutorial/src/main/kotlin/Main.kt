3730d445becec5164328b3177da8a2646d2a3d1e
Shinonome <cuulbulu@gmail.com>
2023-12-20 18:14:01 +0900

    Update: セッターインジェクションについて記載

--- tutorial/src/main/kotlin/Main.kt
+++ tutorial/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 /**
  * https://zenn.dev/tokium_dev/articles/dependency-injection-watanabe
+ * https://irof.hateblo.jp/entry/2017/04/16/222737
  */
 
 // classにコンストラクタ無いときは()省略できるみたい
@@ -11,16 +12,34 @@ class CarNotDi {
     private val engine = Engine()
 
     fun start() {
+        println("not di")
         engine.start()
     }
 }
 
 // コンストラクタインジェクション
-// コンストラクタ: インスタンス化したとき自動で呼び出される機能
-class CarDi(private val engine: Engine) {
+class CarConstDi(
+    // コンストラクタ: インスタンス化したとき自動で呼び出される機能
+    private val engine: Engine
+) {
     fun start() {
+        println("const")
         engine.start()
     }
+}
+
+// セッターインジェクション
+class CarSetterDi{
+    private lateinit var engine: Engine
+    fun setter(setEngine: Engine) {
+        this.engine = setEngine
+    }
+
+    fun start() {
+        println("setter")
+        engine.start()
+    }
+
 }
 
 class Engine {
@@ -29,12 +48,18 @@ class Engine {
     }
 }
 
-
 fun main(args: Array<String>) {
     val carNotDi = CarNotDi()
     carNotDi.start()
 
     val engine = Engine()
-    val car = CarDi(engine)
-    car.start()
+
+    // コンストラクタインジェクション
+    val constCar = CarConstDi(engine)
+    constCar.start()
+
+    // セッターインジェクション
+    val setterCar = CarSetterDi()
+    setterCar.setter(engine)
+    setterCar.start()
 }
