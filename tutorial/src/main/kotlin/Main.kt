75b16d952307097f8cafcea68578799ac81b7260
Shinonome <cuulbulu@gmail.com>
2023-12-21 11:15:41 +0900

    Update: コンストラクタについて書いてる

--- tutorial/src/main/kotlin/Main.kt
+++ tutorial/src/main/kotlin/Main.kt
@@ -18,10 +18,33 @@ class CarNotDi {
 }
 
 // コンストラクタインジェクション
+
+/**
+ * コンストラクタ: インスタンス化したとき自動で呼び出される機能
+ * プライマリコンストラクタ：クラスに1つだけ
+ * class クラス名 constructor(引数: データ型) {}
+ * constructorは省略可
+ */
+
 class CarConstDi(
-    // コンストラクタ: インスタンス化したとき自動で呼び出される機能
-    private val engine: Engine
+    // engineのコンストラクタ引数はプロパティの宣言と初期化を省略している
+    private val engine: Engine,
+    // constTireは通常のコンストラクタの書き方
+    constTire: Tire
 ) {
+    /**
+     * セカンダリコンストラクタ：クラスに0個以上、最終的に必ずプライマリコンストラクタを呼ぶ必要がある
+     * constructor(引数: データ型): this(引数)
+     *
+     */
+    constructor(secondConstEngine: Engine) : this(engine = Engine(), Tire("Confort"))
+
+    private var tire: Tire
+    init {
+        // init{}内でコンストラクタ引数にアクセスできる
+        this.tire = constTire
+    }
+
     fun start() {
         println("const")
         engine.start()
@@ -48,14 +71,17 @@ class Engine {
     }
 }
 
+class Tire(val type: String) {}
+
 fun main(args: Array<String>) {
     val carNotDi = CarNotDi()
     carNotDi.start()
 
     val engine = Engine()
+    val tire = Tire("Sport")
 
     // コンストラクタインジェクション
-    val constCar = CarConstDi(engine)
+    val constCar = CarConstDi(engine, tire)
     constCar.start()
 
     // セッターインジェクション
