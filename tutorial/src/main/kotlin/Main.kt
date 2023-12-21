73940f30bdf18ab4baed01a63177fb59ad4f43c0
Shinonome <cuulbulu@gmail.com>
2023-12-21 18:31:19 +0900

    Update: コンストラクタ周りを整理

--- tutorial/src/main/kotlin/Main.kt
+++ tutorial/src/main/kotlin/Main.kt
@@ -3,22 +3,6 @@
  * https://irof.hateblo.jp/entry/2017/04/16/222737
  */
 
-// classにコンストラクタ無いときは()省略できるみたい
-class CarNotDi {
-    /**
-     * Carのインスタンスを動かす（start関数を呼ぶ）ためにEngineのインスタンスが不可欠
-     * 「CarはEngineに依存している」
-     */
-    private val engine = Engine()
-
-    fun start() {
-        println("not di")
-        engine.start()
-    }
-}
-
-// コンストラクタインジェクション
-
 /**
  * コンストラクタ: インスタンス化したとき自動で呼び出される機能
  * プライマリコンストラクタ：クラスに1つだけ
@@ -26,23 +10,16 @@ class CarNotDi {
  * constructorは省略可
  */
 
-class CarConstDi(
+class Car(
     // engineのコンストラクタ引数はプロパティの宣言と初期化を省略している
     private val engine: Engine,
-    // constTireは通常のコンストラクタの書き方
-    constTire: Tire
+    private val tire: Tire
 ) {
     /**
      * セカンダリコンストラクタ：クラスに0個以上、最終的に必ずプライマリコンストラクタを呼ぶ必要がある
      * constructor(引数: データ型): this(引数)
      */
     constructor(engine: Engine) : this(engine = Engine(), Tire("Comfort"))
-
-    private var tire: Tire
-    init {
-        // init{}内でコンストラクタ引数にアクセスできる
-        this.tire = constTire
-    }
 
     fun start() {
         println("const")
@@ -61,7 +38,6 @@ class Engine {
 }
 
 class Tire(private val type: String) {
-    // openつけるとオーバーライド可能なメソッド
     fun type() {
         println("This tire is $type")
     }
@@ -69,23 +45,15 @@ class Tire(private val type: String) {
 
 
 fun main(args: Array<String>) {
-    val carNotDi = CarNotDi()
-    carNotDi.start()
-
     val engine = Engine()
     val tire = Tire("Sport")
 
-    // コンストラクタインジェクション
-    // プライマリコンストラクタ呼んでる
-    val constCar = CarConstDi(engine, tire)
+    val constCar = Car(engine, tire)
     constCar.start()
     constCar.tireType()
 
-    // セカンダリコンストラクタ呼んでる
-    println("secondConst")
-    val secondConstCar = CarConstDi(engine)
+    val secondConstCar = Car(engine)
     secondConstCar.start()
     secondConstCar.tireType()
-
 
 }
